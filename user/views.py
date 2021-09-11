528ea5e2b1c83030c1c6fe9172608cb7964e57b9
YEOJU-SEOK <yeoju.ac@gmail.com>
2021-09-12 00:51:20 +0900

    add : serializer&views 추가

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from rest_framework import permissions, status, generics
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProfileSerializer, UserSerializerWithToken, UserSerializer
+from .models import Profile
 
-# Create your views here.
+
+class CurrentUserAPIView(RetrieveAPIView):
+    """
+    현재 유저 호출 APIView
+    """
+    serializer_class = ProfileSerializer
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data)
+
