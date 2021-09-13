8f0fe49e5cbff459ea1d00c19fc081f91b8b8222
YEOJU-SEOK <yeoju.ac@gmail.com>
2021-09-13 22:36:40 +0900

    add : user urls 추가

--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,6 @@ from .views import CurrentUserAPIView, UserListAPIView, ProfileUpdateAPIView
 urlpatterns = [
     path('currentuser/', CurrentUserAPIView.as_view()),
     path('userlist/', UserListAPIView.as_view()),
-<<<<<<< HEAD
     path("profileupdate/<int:user_pk>/", ProfileUpdateAPIView.as_view()),
-=======
-    path("profileupdate/<int:user_pk>ㅊㅊ/", ProfileUpdateAPIView.as_view()),
->>>>>>> d5f2d071420f2d770d2d9ec5da46f48b06540f2d
+
 ]
