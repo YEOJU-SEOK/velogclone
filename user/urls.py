caa5432a010ba037c80f257ee5246c07421d38e0
YEOJU-SEOK <yeoju.ac@gmail.com>
2021-09-13 22:34:31 +0900

    add : user urls 추가

---
+++ user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import CurrentUserAPIView, UserListAPIView, ProfileUpdateAPIView
+
+urlpatterns = [
+    path('currentuser/', CurrentUserAPIView.as_view()),
+    path('userlist/', UserListAPIView.as_view()),
+    path("profileupdate/<int:user_pk>/", ProfileUpdateAPIView.as_view()),
+]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from rest_framework import permissions, status, generics
-from rest_framework.generics import RetrieveAPIView
+from rest_framework import permissions, status
+from rest_framework.generics import RetrieveAPIView, CreateAPIView, UpdateAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from .serializers import ProfileSerializer, UserSerializerWithToken, UserSerializer
 from .models import Profile
 
@@ -19,3 +18,28 @@ class CurrentUserAPIView(RetrieveAPIView):
         serializer = self.serializer_class(request.user)
         return Response(serializer.data)
 
+
+#TODO 회원가입으로 class명 수정예정
+class UserListAPIView(CreateAPIView):
+    """
+    회원가입 APIView(with TOKEN)
+    """
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserSerializerWithToken
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#TODO 목적에 맞는 class명 수정
+class ProfileUpdateAPIView(UpdateAPIView):
+    """
+    프로필 업데이트 APIView
+    """
+    lookup_field = "user_pk"
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
