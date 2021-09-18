2b3589bb56d59c7acd9795ff886e0da557538814
YEOJU-SEOK <yeoju.ac@gmail.com>
2021-09-18 23:46:55 +0900

    add : component 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navi from './components/Navi';
+import Header from './components/Header';
+import LoginModal from './components/LoginModal';
+import { Route } from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="App">
+        <div className="auto-margin">
+        
+          <Route exact path="/">
+            <Header modal={modal}/>
+          </Route>
+
+          <Route exact path="/">
+            <Navi/>
+          </Route>
+
+          <Route exact path="/login">
+            <LoginModal setModal={setModal}/>
+          </Route>
+          
+      </div>
     </div>
+    </>
   );
 }
 
